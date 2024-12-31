e5f3e06ebbe393b2743ecc6eddd4ed5e1e7ad061
aspoorna24 <aspoorna24@gmail.com>
2024-12-31 11:53:24 +0530

    Adding Proper URL

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,13 @@ import { useEffect, useState } from "react";
 function App() {
   const [message, setMessage] = useState("");
   
-  useEffect(()=>{
-    fetch("http://localhost:8000/message")
+  
+    fetch("https://app-two-opal.vercel.app/message")
     .then((res) =>res.json())
     .then((data)=>setMessage(data.message));
-  }, []);
+ 
+
+ 
 
   return (
     <div><h1>
